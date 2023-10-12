58adfa6cc3afdc20728b1b9f7323f961641e10cf
yuzhoufen <yuzhoufen@foresee.com.cn>
2023-10-12 17:41:59 +0800

    ttt

--- api/index.js
+++ api/index.js
@@ -43,7 +43,7 @@ const express = require("express"),
   }
 }); */
 app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
+  const path = `/api/item/22`;
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
